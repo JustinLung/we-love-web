e37e2a1c542c88416d31327704805fb9b94c7899
JustinLung <justinlung77@gmail.com>
2022-09-17 23:50:49 +0200

    added hover & focus state to button

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -40,13 +40,28 @@
 		all: unset;
 		background-color: var(--color-light-blue);
 		color: var(--color-white);
-		padding: .5rem 1rem;
+		border: 1px solid var(--color-light-blue);
+		padding: 0.5rem 1rem;
 		font-size: 1.2rem;
-		border-radius: .3rem;
-		margin-top: .5rem;
+		border-radius: 0.3rem;
+		margin-top: 0.5rem;
+		cursor: pointer;
+		transition: 0.3s ease-in-out;
 	}
 
-	@media(max-width: 50rem) {
+	button:hover {
+		background-color: var(--color-white);
+		color: var(--color-light-blue);
+		border: 1px solid var(--color-light-blue);
+	}
+
+	button:focus {
+		background-color: var(--color-white);
+		color: var(--color-light-blue);
+		border: 1px solid var(--color-light-blue);
+	}
+
+	@media (max-width: 50rem) {
 		section {
 			flex-direction: column;
 			align-items: flex-start;
