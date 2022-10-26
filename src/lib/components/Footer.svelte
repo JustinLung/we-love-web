38a0d3afd04140dd113ec74ef615230d14d216e5
JustinLung <justinlung77@gmail.com>
2022-10-26 13:34:03 +0200

    footer updated

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer>
+	&#169; {new Date().getFullYear()}
+	<a href="https://justinlung.com/" target="_blank">Justin Lung</a> &#183; All Rights Reserved
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		margin: 2rem auto;
+		font-size: var(--font-size-normal);
+	}
+
+	a {
+		text-decoration: underline;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import '../styles/style.css';
 	import '../styles/theme.css';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <main>
 	<slot />
 </main>
+<Footer />
